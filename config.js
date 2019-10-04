66e43e6d557abdb25b61b248594a2d8daac584d4
comdet <listzone@hotmail.com>
2019-10-04 23:35:42 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'esp32-wroom',
+    platform : 'arduino-esp32',
+    title : 'Generic ESP32 Wroom',
+    description : 'ESP32-Wroom development board (Compatible with all ESP32 board).\n',
+    author : 'Comdet Phueadphut',
+    website : 'https://docs.espressif.com/projects/esp-idf/en/latest/hw-reference/get-started-devkitc.html',
+    email : 'comdet.p@gmail.com',
+    git : 'https://github.com/comdet/generic-esp32-wroom/',
+    image : '/static/display.jpg',
+    version : '1.0.0'
+};

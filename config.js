87b8bd886b183228af6d9788bb2b29a4133c8d02
Max <sonthaya.nt@hotmail.com>
2020-10-13 22:10:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
     title : 'IOXESP32',
     description : 'ESP32 development board.\n',
     author : 'Comdet Phueadphut | Modify by Sonthaya.N',
-    website : 'https://ioxesp32.ioxhop.info/',
-    email : 'max30012540@hotmail.com',
+    website : 'https://docs.ioxesp32.com/',
+    email : 'sonthaya.nt@hotmail.com',
     git : 'https://github.com/ioxhop/kbide-IOXESP32-board/',
     image : '/static/display.jpg',
-    version : '1.0.0'
+    version : '2.0.0'
 };
